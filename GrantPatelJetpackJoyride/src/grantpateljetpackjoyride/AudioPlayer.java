62615ba330924770bcb663c657db893bc76e3807
EhsanPatel <EhsanPatel@users.noreply.github.com>
2021-04-06 14:30:32 -0400

    Fixed sound player for jar

--- GrantPatelJetpackJoyride/src/grantpateljetpackjoyride/AudioPlayer.java
+++ GrantPatelJetpackJoyride/src/grantpateljetpackjoyride/AudioPlayer.java
@@ -5,43 +5,60 @@
  */
 package grantpateljetpackjoyride;
 
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author cogra9807
  */
 public class AudioPlayer {
-    Clip clip;
     
-    AudioInputStream audioInputStream;
+    Clip audioClip;
+    AudioInputStream audioStream;
     
     public AudioPlayer(String filepath)
             throws UnsupportedAudioFileException,
         IOException, LineUnavailableException 
     {
-        // create AudioInputStream object
-        audioInputStream = AudioSystem.getAudioInputStream(new File(filepath).getAbsoluteFile());
-          
-        // create clip reference
-        clip = AudioSystem.getClip();
-          
-        // open audioInputStream to the clip
-        clip.open(audioInputStream);
-          
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
         
-        clip.start();
+        //reads the wav file into a buffered input stream to use (works for both netbeans and jar)
+        BufferedInputStream ais = new BufferedInputStream(MainGUI.class.getResourceAsStream(filepath));
+
+        //reads the audio stream from the input stream
+        audioStream = AudioSystem.getAudioInputStream(ais);
+        
+        //gets the format to pass into the dataline object
+        AudioFormat format = audioStream.getFormat();
+        DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+        //gets the audio clip to play from the line
+        audioClip = (Clip) AudioSystem.getLine(info);
+
+        //the audio clip opens the stream to read from
+        audioClip.open(audioStream);
+        //loops continuously
+        audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+        //starts playing the audio
+        audioClip.start();
     }
     
     public void stop(){
-        clip.stop();
-        clip.close();
+        //stops the clip
+        audioClip.stop();
+        try{
+            //closes the stream to free resources
+            audioStream.close();
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(null,e);
+        }
     }
 }
