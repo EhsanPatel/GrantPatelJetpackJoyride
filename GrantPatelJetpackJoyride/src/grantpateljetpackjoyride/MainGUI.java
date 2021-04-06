d03a69a5af8242f6e933015d8d98161a95ca2641
cogra9807 <cogra9807@penguin>
2021-04-05 20:41:39 -0400

    committed by colin

--- GrantPatelJetpackJoyride/src/grantpateljetpackjoyride/MainGUI.java
+++ GrantPatelJetpackJoyride/src/grantpateljetpackjoyride/MainGUI.java
@@ -52,8 +52,8 @@ public class MainGUI extends JPanel implements ActionListener, KeyListener, Mous
     
     boolean mainMusicPlaying = false;
     boolean menuMusicPlaying = true;
-    private String filepathMain = "src/grantpateljetpackjoyride/audio/JetpackJoyrideOST-MainTheme.wav";
-    private String filepathMenu = "src/grantpateljetpackjoyride/audio/JetpackJoyrideOST-Home.wav";
+    private final String filepathMain = "src/grantpateljetpackjoyride/audio/JetpackJoyrideOST-MainTheme.wav";
+    private final String filepathMenu = "src/grantpateljetpackjoyride/audio/JetpackJoyrideOST-Home.wav";
     private AudioPlayer audioPlayer;
     
     
